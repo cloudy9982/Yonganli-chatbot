457346d467020a32ad8f7f89337e502916433478
cloudy9982 <108703022@g.nccu.edu.tw>
2022-06-27 13:34:04 +0800

    Latest News template complete

--- src/api.ts
+++ src/api.ts
@@ -1,69 +1,23 @@
-const marketUrl = process.env.MARKET_API;
+const sensorUrl = process.env.SENSOR_API;
 const key = process.env.KEY;
 const crypto = require('crypto');
 const fetch = require('node-fetch');
 
-export async function fetchOrders(phone: string, name: string) {
-  const body = JSON.stringify({
-    purchaser_mobile: phone,
-    line_user_id: name
-  });
-  const payload = Buffer.from(body);
-  const sign = crypto.createHmac('sha1', key).update(payload).digest('hex');
-
-  return fetch(marketUrl, {
-    method: 'POST',
+export async function fetchSensorData() {
+  return fetch(`${sensorUrl}?params=soil_temperature%2Csoil_moisture%2Csoil_conductivity`, {
     headers: {
-      'Content-Type': 'application/json; charset=utf-8',
-      'x-hub-signature': `sha1=${sign}`
-    },
-    body
-  })
-    .then((x: any) => x.json())
-    .catch(console.log);
-}
-
-export async function fetchKeywords() {
-  return fetch('https://icook.tw/api/v1/keywords/hot_keywords', {
-    headers: {
-      'User-Agent': 'tw.icook.chatbot'
+      'Authorization': `Bearer ${key}`
     }
   })
     .then((x: any) => x.json())
     .catch(console.log);
 }
 
-export async function fetchHitfood(keyword: string) {
-  return fetch(
-    `https://icook.tw/api/v1/recipes/search.json?q=${encodeURIComponent(
-      keyword
-    )}`,
-    {
-      headers: {
-        'User-Agent': 'tw.icook.chatbot'
-      }
-    }
-  )
-    .then((x: any) => x.json())
-    .catch(console.log);
-}
-
-export async function fetchSeasonfood() {
-  return fetch('https://icook.tw/api/v1/homepage_v2.json', {
-    headers: {
-      'User-Agent': 'tw.icook.chatbot'
-    }
-  })
-    .then((x: any) => x.json())
-    .catch(console.log);
-}
-
-export async function fetchRecipe(id: string) {
-  return fetch(`https://icook.tw/api/v1/recipes/${id}.json`, {
-    headers: {
-      'User-Agent': 'tw.icook.chatbot'
-    }
-  })
-    .then((x: any) => x.json())
-    .catch(console.log);
-}
+// fetch('https://data.agriweather.online/api/v1/devices/FDxjm4ej/realtime?params=soil_temperature%2Csoil_moisture%2Csoil_conductivity', {
+//   headers: {
+//     'Authorization': 'Bearer TzJKZDBJWVhNdXNZVnEwNjZpS204MGpzZHVsTzc5emFGcGFQM000NkVsRmhrS2l0b3BPYU9KaVI4Z2Nv62b7056d60ec1'
+//   }
+// })
+//    .then((x) => x.json())
+//     .then((y) => console.log(y))
+//   .catch(console.log);
